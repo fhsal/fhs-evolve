e00791f5df28c708b15c41c1f5451e4962222310
frank salvo <fhsalvo@yahoo.com>
2021-01-28 22:11:20 -0500

    attempting to revert to prior version

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 const express = require("express");
 
-// const redirectSSL = require('redirect-ssl')
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-
-// app.use(redirectSSL.create({
-//   enabled: process.env.NODE_ENV === 'production'
-// }))
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
@@ -38,4 +31,3 @@ mongoose.connect(
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
-
