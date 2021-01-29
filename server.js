310d63500541a8060560cccb9f8a807faa53988b
frank salvo <fhsalvo@yahoo.com>
2021-01-28 22:24:26 -0500

    trying again to fix https

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+// const redirectSSL = require('redirect-ssl')
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
+// app.use(redirectSSL)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
