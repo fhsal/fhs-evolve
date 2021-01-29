c9940cfa288c44c548b290f7754aa111996864b0
frank salvo <fhsalvo@yahoo.com>
2021-01-28 21:31:13 -0500

    adding ssl redirect

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
+const redirectSSL = require('redirect-ssl')
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
+
+app.use(redirectSSL.create({
+  enabled: process.env.NODE_ENV === 'production'
+}))
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
