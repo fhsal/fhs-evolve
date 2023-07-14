1cefcfaca4edcfd13f0364cd5c0517f020e40786
F.  Salvo <68975104+fhsal@users.noreply.github.com>
2023-07-14 12:01:37 -0400

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,7 +6,7 @@ console.log(apiKey)
 
 export default {
   getQuote: () =>
-  axios({
+  axios(
       'method':'GET',
       'url': 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote',
       'params': {
@@ -14,6 +14,5 @@ export default {
       'headers': {
           'x-rapidapi-host':'quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com',
           'x-rapidapi-key': process.env.REACT_APP_API_KEY
-      },
     )
 }
