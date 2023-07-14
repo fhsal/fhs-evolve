ad723152e93bba742de63727bd20a9d98fcd599a
F.  Salvo <68975104+fhsal@users.noreply.github.com>
2023-07-14 12:18:46 -0400

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,13 +5,6 @@ const apiKey = process.env.REACT_APP_API_KEY
 console.log(apiKey)
 
 export default {
-  getQuote: () =>
-  axios(
-      'method':'GET',
-      'url': 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote',
-      'params': {
-      'token': 'ipworld.info'},
-      'headers': {
-          'x-rapidapi-host':'quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com',
-          'x-rapidapi-key': process.env.REACT_APP_API_KEY
-      })}
+    getQuote: function() {
+        return axios.get("https://zenquotes.io/api/random");
+        },
