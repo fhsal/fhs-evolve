918706970be5bf224a1084e57b145b4be33bd83e
F.  Salvo <68975104+fhsal@users.noreply.github.com>
2023-07-14 10:43:42 -0400

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,15 +8,14 @@ export default {
   getQuote: () =>
   axios({
       'method':'GET',
-  url: 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote',
-  params: {
-    token: 'ipworld.info'
-  },
+  'url': 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote',
+  'params': {
+    'token': 'ipworld.info'}
 
     
     // 'url':'https://quotable-quotes.p.rapidapi.com/randomQuotes',
       'headers': {
-          'x-rapidapi-host':'quotable-quotes.p.rapidapi.com',
+          'x-rapidapi-host':'quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com',
           'x-rapidapi-key': process.env.REACT_APP_API_KEY
       },
   })
