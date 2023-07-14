82a3bb8a22876a466da1f30c541f18ceb84500e7
F.  Salvo <68975104+fhsal@users.noreply.github.com>
2023-07-14 12:45:56 -0400

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,9 +2,17 @@ import axios from "axios";
 
 
 const apiKey = process.env.REACT_APP_API_KEY
-console.log(apiKey)
 
-export default {
-    getQuote: function() {
-        return axios.get("https://zenquotes.io?api=random");
+
+      export default {
+        getQuote: function() {
+          axios("https://zenquotes.io?api=random", {
+            method: 'GET',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+                'Authorization': key,
+                withCredentials: true,
+                mode: 'no-cors',
+              }})
         }}
