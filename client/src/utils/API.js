c49c604410bdb16b990e81ad8a58e30a5d66afbf
F.  Salvo <68975104+fhsal@users.noreply.github.com>
2023-07-14 11:44:37 -0400

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,7 +10,7 @@ export default {
       'method':'GET',
       'url': 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote',
       'params': {
-      'token': 'ipworld.info'}
+      'token': 'ipworld.info'},
       'headers': {
           'x-rapidapi-host':'quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com',
           'x-rapidapi-key': process.env.REACT_APP_API_KEY
