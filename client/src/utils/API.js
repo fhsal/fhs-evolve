9f259377ae9850c4ad8c094c321a01ddf023c1b3
F.  Salvo <68975104+fhsal@users.noreply.github.com>
2023-07-14 12:52:51 -0400

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,7 +10,7 @@ const apiKey = process.env.REACT_APP_API_KEY
             method: 'GET',
             headers: {
                 'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json,
+                'Content-Type': 'application/json',
                 mode: 'no-cors',
               }})
         }}
