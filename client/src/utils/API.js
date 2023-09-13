54a0f799718f704c904deab812354756f1d09144
F.  Salvo <68975104+fhsal@users.noreply.github.com>
2023-09-13 11:13:35 -0400

    adding author split

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,19 +5,5 @@ export default {
     return axios.get("https://type.fit/api/quotes");
   },
 
-<<<<<<< HEAD
-    // Saves a week of Health information to the database
-    saveHealth: function(healthData) {
-      return axios.post("/api/health", healthData);
-    }
-
   
 };
-      
-
-
- 
-=======
-  
-};
->>>>>>> 092151e234136968295eddbe8a25009b306d906b
